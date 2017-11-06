c488f53226ec4b5cc387ff26e0c4184fdc5ce636
majdeddine <majda-jambouri@hotmail.fr>
2017-11-06 15:53:19 +0000

    add views

--- app.rb
+++ app.rb
@@ -9,13 +9,5 @@ get '/secret' do
 end
 
 get '/cat' do
-
-  "<style>
-  .red-border {
-    border-color: red;
-    border-width: 5px;
-    border-style: dashed;
-  }
-</style>
-  <img class=red-border src= http://bit.ly/1eze8aE >"
+  erb(:index)
 end
